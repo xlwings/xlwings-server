5fe9144ce8db443dcc9b51ad044746c7e6cd89b4
Felix Zumstein <fzumstein@gmail.com>
2024-04-01 11:49:01 +0200

    introduces base template and xlwings-alert

---
+++ app/static/js/xlwings-alert.js
@@ -0,0 +1,22 @@
+if (document.getElementById("ok")) {
+  document.getElementById("ok").addEventListener("click", buttonCallback);
+}
+if (document.getElementById("yes")) {
+  document.getElementById("yes").addEventListener("click", buttonCallback);
+}
+if (document.getElementById("no")) {
+  document.getElementById("no").addEventListener("click", buttonCallback);
+}
+if (document.getElementById("cancel")) {
+  document.getElementById("cancel").addEventListener("click", buttonCallback);
+}
+async function buttonCallback() {
+  Office.onReady(function (info) {
+    // This causes this issue: https://github.com/OfficeDev/office-js/issues/3582
+    Office.context.ui.messageParent(
+      this.id +
+        "|" +
+        document.getElementById("callback").getAttribute("data-callback"),
+    );
+  });
+}
