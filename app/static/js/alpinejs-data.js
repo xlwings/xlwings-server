446c43a80a2595757c258dd09defaf8644c46ece
Felix Zumstein <fzumstein@gmail.com>
2024-04-01 11:52:48 +0200

    refactor js

--- app/static/js/alpinejs-data.js
+++ app/static/js/alpinejs-data.js
@@ -1,13 +1,12 @@
 const data = {
   bookName: {
     getName: async function () {
-      await Office.onReady(); // TODO: move to xlwings.js
       return await xlwings.getActiveBookName();
     },
   },
 };
 
-// Alpine.data boilerplate
+// Alpine boilerplate
 document.addEventListener("alpine:init", () => {
   for (let name in data) {
     Alpine.data(name, () => data[name]);
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,10 +1,27 @@
+// Socket.io
+try {
+  globalThis.socket = io({
+    auth: async (callback) => {
+      let token = await globalThis.getAuth();
+      callback({
+        token: token,
+      });
+    },
+  });
+} catch (error) {
+  console.error(error);
+  globalThis.socket = null;
+}
+
 // Sample 1: Button on Task pane
 // Use runPython with the desired endpoint of your web app
 let helloButton = document.querySelector("[data-js-hello]");
-helloButton.addEventListener("click", async function (event) {
-  let token = await globalThis.getAuth();
-  xlwings.runPython(window.location.origin + "/hello", { auth: token });
-});
+if (helloButton) {
+  helloButton.addEventListener("click", async function (event) {
+    let token = await globalThis.getAuth();
+    xlwings.runPython(window.location.origin + "/hello", { auth: token });
+  });
+}
 
 // Sample 2: Button on Ribbon
 // Needs event.completed() and Office.actions.associate
