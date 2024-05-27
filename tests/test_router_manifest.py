f2e9c0400b52c8c1b96cf9464f58daa4af78fd95
Felix Zumstein <fzumstein@gmail.com>
2024-05-27 11:51:59 +0200

    added more manifest tests

--- tests/test_router_manifest.py
+++ tests/test_router_manifest.py
@@ -39,3 +39,19 @@ def test_empty_function_namespace_nonprod(monkeypatch):
     monkeypatch.setattr(settings, "functions_namespace", "")
     response = client.get("/manifest")
     assert '<bt:String id="Functions.Namespace" DefaultValue="QA" />' in response.text
+
+
+def test_entraid_auth_deactivated(monkeypatch):
+    response = client.get("/manifest")
+    assert "<WebApplicationInfo>" not in response.text
+
+
+def test_entraid_auth_activated(monkeypatch):
+    monkeypatch.setattr(
+        settings, "entraid_client_id", "5be64098-090e-4183-ac32-e35f09379bb1"
+    )
+    monkeypatch.setattr(
+        settings, "entraid_tenant_id", "efadd9e3-83b3-41f8-86d1-c0ea7ad203ad"
+    )
+    response = client.get("/manifest")
+    assert "<WebApplicationInfo>" in response.text
