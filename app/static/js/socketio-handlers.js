102c5434bf7e36aa578493cf68666c66cb25fcf3
Felix Zumstein <fzumstein@gmail.com>
2024-06-17 22:30:42 +0200

    added utils.trigger_script (#42)
    * added utils.trigger_script
    
    * fix test

--- app/static/js/socketio-handlers.js
+++ app/static/js/socketio-handlers.js
@@ -12,3 +12,18 @@ try {
   console.log("Didn't load socket.io: ", error);
   globalThis.socket = null;
 }
+
+globalThis.socket.on("xlwings:trigger-script", async (data) => {
+  const appPathElement = document.getElementById("app-path");
+  const appPath = appPathElement
+    ? JSON.parse(appPathElement.textContent)
+    : null;
+  let token = await globalThis.getAuth();
+  xlwings.runPython(
+    window.location.origin +
+      (appPath && appPath.appPath !== "" ? `/${appPath.appPath}` : "") +
+      "/xlwings/custom-scripts-call/" +
+      data.script_name,
+    { ...data.config, auth: token },
+  );
+});
