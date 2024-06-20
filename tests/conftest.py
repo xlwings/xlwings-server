9da4f5ab7bd1614cbde6617da0eca7e5df8371b2
Felix Zumstein <fzumstein@gmail.com>
2024-06-20 22:37:37 +0200

    auth tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,14 @@
 from pathlib import Path
 
+import pytest
 from dotenv import load_dotenv
 
 base_dir = Path(__file__).resolve().parent
 load_dotenv(base_dir / ".env.test", override=True)
 
 # Don't import app before this point or settings won't be overridden
+
+
+@pytest.fixture
+def anyio_backend():
+    return "asyncio"
