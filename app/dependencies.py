1bb8141cc43227dcef30c809ef41711b6c660ddd
Felix Zumstein <fzumstein@gmail.com>
2024-06-25 09:58:48 +0200

    refactor get_user dependency to make it testable

--- app/dependencies.py
+++ app/dependencies.py
@@ -56,37 +56,25 @@ async def authenticate(
     return current_user
 
 
-class Authorizer:
-    """This class can be used to create dependencies that require specific roles:
-
-    get_specific_user = Authorizer(roles=["role1", "role2"])
-
-    Returns a user object.
-    """
-
-    def __init__(self, required_roles: list = None):
-        self.required_roles = required_roles
-
-    async def __call__(self, current_user: models.User = Depends(authenticate)):
-        if not settings.auth_providers:
-            return current_user
-        # RBAC
-        has_required_roles = await current_user.has_required_roles(self.required_roles)
-        if not has_required_roles:
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail=f"Auth error: Missing roles for {current_user.name}: {', '.join(set(self.required_roles).difference(current_user.roles))}",
-            )
-        # Authorization
-        if not await current_user.is_authorized():
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="Auth error: Not authorized.",
-            )
+async def get_user(current_user: models.User = Depends(authenticate)):
+    if not settings.auth_providers:
         return current_user
-
-
-get_user = Authorizer(required_roles=settings.auth_required_roles)
+    # RBAC
+    has_required_roles = await current_user.has_required_roles(
+        settings.auth_required_roles
+    )
+    if not has_required_roles:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"Auth error: Missing roles for {current_user.name}: {', '.join(set(settings.auth_required_roles).difference(current_user.roles))}",
+        )
+    # Authorization
+    if not await current_user.is_authorized():
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Auth error: Not authorized.",
+        )
+    return current_user
 
 
 User = Annotated[models.User, Depends(get_user)]
