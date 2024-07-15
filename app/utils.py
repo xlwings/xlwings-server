a933148752f656caa7f0a0c4ca05ae7f3f71f717
Felix Zumstein <fzumstein@gmail.com>
2024-07-15 12:03:04 +0200

    Allow to run Socket.io in an own process

--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,17 @@
+import logging
+
 from .routers import socketio as socketio_router, xlwings as xlwings_router
+
+logger = logging.getLogger(__name__)
 
 
 async def trigger_script(script, **options):
+    sid = xlwings_router.socketio_id_context.get()
     if not isinstance(script, str):
         script = script.__name__
     await socketio_router.sio.emit(
         "xlwings:trigger-script",
         {"script_name": script, "config": options},
-        to=xlwings_router.socketio_id_context.get(),
+        to=sid,
     )
+    logger.info(f"Script '{script}' triggered for sid '{sid}' with config: {options}")
