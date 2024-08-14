e01e9a36f56093d4f683c9ea760549233e0283e0
Felix Zumstein <fzumstein@gmail.com>
2024-08-14 22:18:29 +0200

    added staging environment

--- app/config.py
+++ app/config.py
@@ -31,7 +31,7 @@ class Settings(BaseSettings):
     enable_excel_online: bool = True
     enable_htmx: bool = True
     enable_socketio: bool = True
-    environment: Literal["dev", "qa", "uat", "prod"] = "prod"
+    environment: Literal["dev", "qa", "uat", "staging", "prod"] = "prod"
     functions_namespace: str = "XLWINGS"
     hostname: Optional[str] = None
     log_level: str = "INFO"
@@ -39,6 +39,7 @@ class Settings(BaseSettings):
     manifest_id_dev: UUID4 = "0a856eb1-91ab-4f38-b757-23fbe1f73130"
     manifest_id_qa: UUID4 = "9cda34b1-af68-4dc6-b97c-e63ef6284671"
     manifest_id_uat: UUID4 = "70428e53-8113-421c-8fe2-9b74fcb94ee5"
+    manifest_id_staging: UUID4 = "34041f4f-9cb4-4830-afb5-db44b2a70e0e"
     manifest_id_prod: UUID4 = "4f342d85-3a49-41cb-90a5-37b1f2219040"
     project_name: str = "xlwings Server"
     public_addin_store: bool = False
--- run.py
+++ run.py
@@ -20,6 +20,7 @@ def replace_uuids():
         "0a856eb1-91ab-4f38-b757-23fbe1f73130",
         "9cda34b1-af68-4dc6-b97c-e63ef6284671",
         "4f342d85-3a49-41cb-90a5-37b1f2219040",
+        "34041f4f-9cb4-4830-afb5-db44b2a70e0e",
         "70428e53-8113-421c-8fe2-9b74fcb94ee5",
     ]
     with open(file_path, "w") as file:
