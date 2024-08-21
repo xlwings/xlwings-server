6247be360935d1ff77bae1f5f8a631a441a18149
Felix Zumstein <fzumstein@gmail.com>
2024-08-21 16:21:38 +0200

    docs config

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,12 @@ author = "Zoomer Analytics GmbH"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser"]
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autosectionlabel",  # To make easy intra-page links: :ref:`Title`
+    "sphinx_copybutton",
+    "sphinx_design",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
@@ -24,3 +29,23 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "furo"
 html_static_path = ["_static"]
+html_favicon = "_static/favicon.png"
+html_theme_options = {
+    "sidebar_hide_name": True,
+    "light_logo": "logo-light.svg",
+    "dark_logo": "logo-dark.svg",
+    "light_css_variables": {
+        "color-brand-primary": "black",
+        "color-brand-content": "#28a745",
+        "color-sidebar-caption-text": "#28a745",
+        "sidebar-caption-font-size": "1em",
+        "color-announcement-background": "#28a745",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "white",
+        "color-announcement-background": "#28a745",
+    },
+}
+
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
+copybutton_prompt_is_regexp = True
