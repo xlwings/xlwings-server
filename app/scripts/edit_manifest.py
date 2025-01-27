0836fe30200a174ae98cbd67fb9f9e497133c388
Felix Zumstein <fzumstein@gmail.com>
2025-01-27 11:33:53 +0100

    wip

--- app/scripts/edit_manifest.py
+++ app/scripts/edit_manifest.py
@@ -2,70 +2,68 @@ from pathlib import Path
 
 from lxml import etree
 
-NAMESPACES = {
+NSMAP = {
     None: "http://schemas.microsoft.com/office/appforoffice/1.1",
     "bt": "http://schemas.microsoft.com/office/officeappbasictypes/1.0",
     "ov": "http://schemas.microsoft.com/office/taskpaneappversionoverrides",
     "xsi": "http://www.w3.org/2001/XMLSchema-instance",
 }
-NAMESPACE_OV = "http://schemas.microsoft.com/office/taskpaneappversionoverrides"
+NSMAP_NO_ROOT = {k: v for k, v in NSMAP.items() if k is not None}
 
 
 def add_resources_for_button(tree, button_id, label_text, tooltip_text):
-    # Create namespace map without default namespace
-    string_nsmap = {k: v for k, v in NAMESPACES.items() if k is not None}
-
     # Find or create Resources element
-    resources = tree.find(f".//{{{NAMESPACE_OV}}}Resources")
+    resources = tree.find(f".//{{{NSMAP['ov']}}}Resources")
     if resources is None:
-        version_overrides = tree.find(f".//{{{NAMESPACE_OV}}}VersionOverrides")
-        resources = etree.SubElement(version_overrides, f"{{{NAMESPACE_OV}}}Resources")
+        version_overrides = tree.find(f".//{{{NSMAP['ov']}}}VersionOverrides")
+        resources = etree.SubElement(version_overrides, f"{{{NSMAP['ov']}}}Resources")
 
     # Find or create ShortStrings
-    short_strings = resources.find(".//{%s}ShortStrings" % NAMESPACES["bt"])
+    short_strings = resources.find(f".//{{{NSMAP['bt']}}}ShortStrings")
     if short_strings is None:
         short_strings = etree.SubElement(
-            resources, "{%s}ShortStrings" % NAMESPACES["bt"], nsmap=NAMESPACES
+            resources, f"{{{NSMAP['bt']}}}ShortStrings", nsmap=NSMAP
         )
 
     # Find or create LongStrings
-    long_strings = resources.find(".//{%s}LongStrings" % NAMESPACES["bt"])
+    long_strings = resources.find(f".//{{{NSMAP['bt']}}}LongStrings")
     if long_strings is None:
         long_strings = etree.SubElement(
-            resources, "{%s}LongStrings" % NAMESPACES["bt"], nsmap=NAMESPACES
+            resources, f"{{{NSMAP['bt']}}}LongStrings", nsmap=NSMAP
         )
 
     # Add label (short string) with DefaultValue
     etree.SubElement(
         short_strings,
-        "{%s}String" % NAMESPACES["bt"],
+        f"{{{NSMAP['bt']}}}String",
         {"id": f"{button_id}.Label", "DefaultValue": label_text},
-        nsmap=string_nsmap,
+        nsmap=NSMAP_NO_ROOT,
     )
 
     # Add tooltip (long string) with DefaultValue
     etree.SubElement(
         long_strings,
-        "{%s}String" % NAMESPACES["bt"],
+        f"{{{NSMAP['bt']}}}String",
         {"id": f"{button_id}.Tooltip", "DefaultValue": tooltip_text},
-        nsmap=string_nsmap,
+        nsmap=NSMAP_NO_ROOT,
     )
 
 
 def add_button_to_ribbon_group(
     tree, group_id, button_id, function_name, label_text, tooltip_text
 ):
-    # First add the resources
-    add_resources_for_button(tree, button_id, label_text, tooltip_text)
     # Find group using xpath with namespace
     group_elem = tree.find(f".//*[@id='{group_id}']")
     if group_elem is None:
         return False
 
+    # Resources
+    add_resources_for_button(tree, button_id, label_text, tooltip_text)
+
     # Create button element
     new_button = etree.SubElement(
         group_elem,
-        f"{{{NAMESPACE_OV}}}Control",
+        f"{{{NSMAP['ov']}}}Control",
         {
             "{http://www.w3.org/2001/XMLSchema-instance}type": "Button",
             "id": button_id,
@@ -74,35 +72,35 @@ def add_button_to_ribbon_group(
 
     # Add label
     etree.SubElement(
-        new_button, f"{{{NAMESPACE_OV}}}Label", {"resid": f"{button_id}.Label"}
+        new_button, f"{{{NSMAP['ov']}}}Label", {"resid": f"{button_id}.Label"}
     )
 
     # Add Supertip
-    supertip = etree.SubElement(new_button, f"{{{NAMESPACE_OV}}}Supertip")
+    supertip = etree.SubElement(new_button, f"{{{NSMAP['ov']}}}Supertip")
     etree.SubElement(
-        supertip, f"{{{NAMESPACE_OV}}}Title", {"resid": f"{button_id}.Label"}
+        supertip, f"{{{NSMAP['ov']}}}Title", {"resid": f"{button_id}.Label"}
     )
     etree.SubElement(
-        supertip, f"{{{NAMESPACE_OV}}}Description", {"resid": f"{button_id}.Tooltip"}
+        supertip, f"{{{NSMAP['ov']}}}Description", {"resid": f"{button_id}.Tooltip"}
     )
 
     # Add Icon with bt namespace
-    icon = etree.SubElement(new_button, f"{{{NAMESPACE_OV}}}Icon")
+    icon = etree.SubElement(new_button, f"{{{NSMAP['ov']}}}Icon")
     for size in [16, 32, 80]:
         etree.SubElement(
             icon,
-            "{%s}Image" % NAMESPACES["bt"],
+            f"{{{NSMAP['bt']}}}Image",
             {"size": str(size), "resid": f"Icon.{size}x{size}"},
-            nsmap={"bt": NAMESPACES["bt"]},
+            nsmap={"bt": NSMAP["bt"]},
         )
 
     # Add Action
     action = etree.SubElement(
         new_button,
-        f"{{{NAMESPACE_OV}}}Action",
+        f"{{{NSMAP['ov']}}}Action",
         {"{http://www.w3.org/2001/XMLSchema-instance}type": "ExecuteFunction"},
     )
-    etree.SubElement(action, f"{{{NAMESPACE_OV}}}FunctionName").text = function_name
+    etree.SubElement(action, f"{{{NSMAP['ov']}}}FunctionName").text = function_name
 
     return True
 
@@ -110,7 +108,6 @@ def add_button_to_ribbon_group(
 if __name__ == "__main__":
     p = Path(__file__).parent / "../templates/manifest.xml"
 
-    # Parse while preserving docinfo
     parser = etree.XMLParser(
         remove_blank_text=True, remove_comments=False, strip_cdata=False
     )
@@ -121,12 +118,11 @@ if __name__ == "__main__":
         "MyCommandsGroup",
         "MyFunctionButton2",
         "hello-ribbon2",
-        "My Button",  # Label text
-        "Click this button to run the function",  # Tooltip text
+        "My Button",
+        "Click this button to run the function",
     )
 
     if success:
-        # Write back with standalone="yes"
         tree.write(
             str(p),
             encoding="utf-8",
