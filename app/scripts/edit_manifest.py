74d818e7fd019dee2e7cf1dede001b2a5d97ef7e
Felix Zumstein <fzumstein@gmail.com>
2025-01-28 23:25:47 +0100

    wip

--- app/scripts/edit_manifest.py
+++ app/scripts/edit_manifest.py
@@ -53,8 +53,8 @@ def add_button_to_ribbon_group(
     tree, group_id, button_id, function_name, label_text, tooltip_text
 ):
     # Find group using xpath with namespace
-    group_elem = tree.find(f".//*[@id='{group_id}']")
-    if group_elem is None:
+    group_el = tree.find(f".//*[@id='{group_id}']")
+    if group_el is None:
         return False
 
     # Resources
@@ -62,7 +62,7 @@ def add_button_to_ribbon_group(
 
     # Create button element
     new_button = etree.SubElement(
-        group_elem,
+        group_el,
         f"{{{NSMAP['ov']}}}Control",
         {
             "{http://www.w3.org/2001/XMLSchema-instance}type": "Button",
@@ -105,6 +105,37 @@ def add_button_to_ribbon_group(
     return True
 
 
+def move_button_in_group(tree, group_id, button_id, new_position):
+    # Find the group
+    group_el = tree.find(f".//*[@id='{group_id}']")
+    if group_el is None:
+        return False
+
+    # Find the button within the group
+    button = group_el.find(f".//*[@id='{button_id}']")
+    if button is None:
+        return False
+
+    # Get all controls in the group
+    controls = group_el.findall(f"{{{NSMAP['ov']}}}Control")
+    if not controls:
+        return False
+
+    # Validate new position
+    if new_position < 0 or new_position >= len(controls):
+        return False
+
+    # Remove button from current position
+    group_el.remove(button)
+
+    # Insert at new position
+    group_el.insert(new_position, button)
+
+    return True
+
+
+# Usage example:
+
 if __name__ == "__main__":
     p = Path(__file__).parent / "../templates/manifest.xml"
 
@@ -113,14 +144,16 @@ if __name__ == "__main__":
     )
     tree = etree.parse(str(p), parser)
 
-    success = add_button_to_ribbon_group(
-        tree,
-        "MyCommandsGroup",
-        "MyFunctionButton2",
-        "hello-ribbon2",
-        "My Button",
-        "Click this button to run the function",
-    )
+    success = move_button_in_group(tree, "MyCommandsGroup", "MyFunctionButton2", 0)
+
+    # success = add_button_to_ribbon_group(
+    #     tree,
+    #     "MyCommandsGroup",
+    #     "MyFunctionButton2",
+    #     "hello-ribbon2",
+    #     "My Button",
+    #     "Click this button to run the function",
+    # )
 
     if success:
         tree.write(
