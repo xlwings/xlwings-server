137b549caed05c52ebb3231b91a2f512f7bb2a91
Felix Zumstein <fzumstein@gmail.com>
2025-07-10 09:08:16 +0200

    moved to docs

--- app/static/js/ribbon.js
+++ app/static/js/ribbon.js
@@ -1,46 +1,17 @@
 // Button on Ribbon
 // Needs event.completed() and Office.actions.associate
 // "hello-ribbon" is the identifier in manifest.xml
-// Make sure to update the ids in createTabConfig to match the ones used in manifest.xml
 async function helloRibbon(event) {
-  const createTabsConfig = (enabled) => ({
-    tabs: [
-      {
-        id: "MyTab",
-        groups: [
-          {
-            id: "MyCommandsGroup",
-            controls: [
-              {
-                id: "MyFunctionButton",
-                enabled: enabled,
-              },
-            ],
-          },
-        ],
-      },
-    ],
+  let scriptName = "hello_world";
+  let authResult =
+    typeof globalThis.getAuth === "function"
+      ? await globalThis.getAuth()
+      : { token: "", provider: "" };
+  await xlwings.runPython({
+    auth: authResult.token,
+    scriptName: scriptName,
+    headers: { "Auth-Provider": authResult.provider },
   });
-
-  // Disable the button
-  await Office.onReady();
-  await Office.ribbon.requestUpdate(createTabsConfig(false));
-
-  try {
-    let scriptName = "hello_world";
-    let authResult =
-      typeof globalThis.getAuth === "function"
-        ? await globalThis.getAuth()
-        : { token: "", provider: "" };
-    await xlwings.runPython({
-      auth: authResult.token,
-      scriptName: scriptName,
-      headers: { "Auth-Provider": authResult.provider },
-    });
-  } finally {
-    // Enable the button
-    await Office.ribbon.requestUpdate(createTabsConfig(true));
-    event.completed();
-  }
+  event.completed();
 }
 Office.actions.associate("hello-ribbon", helloRibbon);
