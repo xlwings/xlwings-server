d2530da6da333cb5857d8d880c8f3233cb8ad391
Felix Zumstein <fzumstein@gmail.com>
2026-01-09 11:42:49 +0100

    improved tests

--- tests/test_auth_entraid.py
+++ tests/test_auth_entraid.py
@@ -7,9 +7,18 @@ from xlwings_server.auth.entraid import get_key_set, validate_token
 
 @pytest.mark.anyio
 async def test_custom_get_jwks_json(mocker):
+    # Use a valid 2048-bit RSA modulus to avoid SecurityWarning
     mock_get_jwks_json = mocker.patch(
         "xlwings_server.auth.entraid.jwks.get_jwks_json",
-        return_value={"keys": [{"kty": "RSA", "n": "test", "e": "AQAB"}]},
+        return_value={
+            "keys": [
+                {
+                    "kty": "RSA",
+                    "n": "0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMstn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbISD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqbw0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw",
+                    "e": "AQAB",
+                }
+            ]
+        },
     )
     spy_get_jwks_json_default = mocker.spy(
         xlwings_server.auth.entraid, "get_jwks_json_default"
