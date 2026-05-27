434ddb6458992b53e7cc3ef6430dbe4c190e5d88
Felix Zumstein <fzumstein@gmail.com>
2026-05-27 15:24:03 +0200

    Also support HEAD requests for root URL

--- xlwings_server/routers/root.py
+++ xlwings_server/routers/root.py
@@ -9,8 +9,8 @@ router = APIRouter(prefix=settings.app_path)
 logger = logging.getLogger(__name__)
 
 
-# Endpoints
-@router.get("/")
+# Endpoints (@get doesn't automatically support HEAD)
+@router.api_route("/", methods=["GET", "HEAD"])
 async def root():
     # This endpoint could be used for a health check
     return {"status": "ok"}
